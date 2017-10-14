7f160af63a7f7c1aaa7a0e472fe4bbf774fe6b02
Terese Haimberger <tvhtoo@gmail.com>
2017-10-14 15:10:51 +0200

    added flights resource

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 require('coffee-script/register');
 
-var index = require('./routes/index');
+var index = require('./resources/index/router');
+var flights = require('./resources/flights/router');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/flights', flights);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ resources/flights/model.coffee
@@ -0,0 +1,4 @@
+Promise = require 'bluebird'
+
+module.exports.find = ({ origin }) ->
+  return Promise.resolve []
